52d9b207e323d290bf0c2c345215a8f2d955240a
Alberto De Agostini <alberto.deagostini@infinitaslearning.com>
2024-05-22 11:03:40 +0200

    remove page extension

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  // pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 export default nextConfig;
